636162969cc984d9646aec4427c51d48f7e050ed
Mark Bannykh <48414914+bannykhmark1@users.noreply.github.com>
2023-05-05 14:48:41 +0500

    create component header

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,29 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import { RiArrowDownSFill } from "react-icons/ri";
-import { IoSearchOutline } from "react-icons/io";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export default function Header() {
   return (
-    <div className="">
-      <div className="">SteamPay</div>
-      <div>
-        <RxHamburgerMenu className="w-23 h-16" />
-      </div>
-      <div>
-        <ul>
-          <li className="flex">
-            <span>Каталог продукции</span>
-            <RiArrowDownSFill className=" self-end" />
-          </li>
-          <li>Поддержка</li>
-          <li>Мои покупки</li>
-          <li>
-            <IoSearchOutline />
-            <span>Поиск игр</span>
-          </li>
-        </ul>
+    <div className="wrapper">
+      <div className="flex border text-sm uppercase">
+        <div className="flex items-center mr-8 ml-4">Steam Pay</div>
+        <div className="flex w-16 h-16 bg-neutral-100 border items-center justify-center">
+          <RxHamburgerMenu className="w-6 h-8" />
+        </div>
+        <div className="flex items-center">
+          <ul className="flex ml-8">
+            <li className="flex ml-4">
+              <span>Каталог продукции</span>
+              <RiArrowDownSFill className="flex self-center -mt-1 ml-1 w-5 h-5 fill-neutral-300" />
+            </li>
+            <li className="ml-4">Поддержка</li>
+            <li className="ml-4">Мои покупки</li>
+            <li className=" flex ml-4">
+              <AiOutlineSearch className="flex -mt-2 w-6 h-8 fill-orange-600 mr-4" />
+              <span className="">Поиск игр</span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
